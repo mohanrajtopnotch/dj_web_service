114c547070e591cac94b11719f5ae29824f0f2ab
mohanrajtopnotch <mohanraj.topnotch@gmail.com>
2021-04-09 08:59:05 +0530

    Service_Maximum_Completed

--- router/LogRoute.js
+++ router/LogRoute.js
@@ -2,8 +2,8 @@ const Router = require("express").Router();
 const bcrpyt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const DJUSER = require("../dbo/dj_user");
-const AuthMiddleware = require("../middlewares/AuthMware");
-
+const LoggerUtil =require('../utils/LoggerUtil')
+const Env = require("dotenv").config({ path: require("find-config")(".env") });
 Router.post("/register", async (req, res) => {
     try {
         const { username, email, password } = req.body;
@@ -16,9 +16,9 @@ Router.post("/register", async (req, res) => {
         });
 
         const save = await dj_user.save();
-        return res.status(200).json({ success: "User Registerd Sucessfully" });
-    } catch (err) {
-        return res.status(400).json({ error: "please try again sometimes" });
+        return LoggerUtil.response(req, res, save);
+    } catch (error) {
+        return LoggerUtil.error(req, res, 404, error);
     }
 });
 
@@ -39,13 +39,29 @@ Router.post("/login", async (req, res) => {
             _id: find_user._id,
         };
         const { username, email, _id } = find_user;
-        const token = await jwt.sign(jwt_user, process.env.JWT_SECRET_KEY);
-        console.log({ username, email, token, id: _id });
-        return res.status(200).json({ username, email, token, id: _id });
-    } catch (err) {
-        return res.status(400).json({ error: "please try again sometimes" });
+        const token = await jwt.sign(jwt_user,Env.parsed.JWT_SECRET_KEY);
+        console.log({ username, email, token , id: _id });
+        // return res.status(200).json({ username, email, token, id: _id });
+        return LoggerUtil.response(req, res, { username, email, token , id: _id });
+    } catch (error) {
+        return LoggerUtil.error(req, res, 404, error);
     }
 });
 
-Router.get("/checkAuth", AuthMiddleware.checkAuth);
+Router.get("/checkAuth",async (req,res)=>{
+    try {
+        const data = jwt.decode(req.body.token);
+        console.log(data);
+        const IsToken = await jwt.verify(
+            req.body.token,
+            Env.parsed.JWT_SECRET_KEY
+        );
+        if (!IsToken) {
+            throw "Authentication Failed";
+        }
+        return LoggerUtil.response(req, res, IsToken);
+    } catch (error) {
+        return LoggerUtil.error(req, res, 404, error);
+    }
+});
 module.exports = Router;
